5b7e45420ec5c8db60baacacfe9096665f1bac4a
alvynah <alvynahw@gmail.com>
2021-03-01 03:54:06 +0300

    Make the site responsive

--- js/scripts.js
+++ js/scripts.js
@@ -25,12 +25,51 @@ $(document).ready(function() {
         $("#project-management-image").slideDown('1000');
     });
 
+
     $("#portfolio1").mouseover(function() {
         $("#overlayEffect1").show();
     }).mouseout(function() {
         $("#overlayEffect1").hide();
     });
+    $("#portfolio2").mouseover(function() {
+        $("#overlayEffect2").show();
+    }).mouseout(function() {
+        $("#overlayEffect2").hide();
+    });
+    $("#portfolio3").mouseover(function() {
+        $("#overlayEffect3").show();
+    }).mouseout(function() {
+        $("#overlayEffect3").hide();
+    });
+    $("#portfolio4").mouseover(function() {
+        $("#overlayEffect4").show();
+    }).mouseout(function() {
+        $("#overlayEffect4").hide();
+    });
+    $("#portfolio5").mouseover(function() {
+        $("#overlayEffect5").show();
+    }).mouseout(function() {
+        $("#overlayEffect5").hide();
+    });
+    $("#portfolio6").mouseover(function() {
+        $("#overlayEffect6").show();
+    }).mouseout(function() {
+        $("#overlayEffect6").hide();
+    });
+    $("#portfolio7").mouseover(function() {
+        $("#overlayEffect7").show();
+    }).mouseout(function() {
+        $("#overlayEffect7").hide();
+    });
+    $("#portfolio8").mouseover(function() {
+        $("#overlayEffect8").show();
+    }).mouseout(function() {
+        $("#overlayEffect8").hide();
+    });
+
 });
+
+//business Logic
 $(document).ready(function() {
     $("form#contactForm").submit(function(event) {
         event.preventDefault();
