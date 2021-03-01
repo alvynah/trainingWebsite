fdba5e7dbe762a2c4ccff9a6c4388d345cf278c0
alvynah <alvynahw@gmail.com>
2021-03-01 10:33:55 +0300

    DRY code

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,13 @@
+//User Logic
 $(document).ready(function() {
+    /* $(".whatwedoimg").click(function() {
+        $(this).children().slideDown("1000").hide("1000");
+        $(".explanation").show('1000');
+    });
+    $(".explanation").click(function() {
+        $(".explanation").slideUp("1000");
+$(".whatwedoimg").slideDown
+    });*/
 
     $("#design-img").click(function() {
         $("#design-img").slideDown("1000").hide("1000");
@@ -24,50 +33,7 @@ $(document).ready(function() {
         $("#project-management").slideUp('1000');
         $("#project-management-image").slideDown('1000');
     });
-    /*
 
-        $("#portfolio1").mouseover(function() {
-            $("this").addClass("mask");
-            $("#overlayEffect1").show();
-        }).mouseout(function() {
-            $("#overlayEffect1").hide();
-        });
-        $("#portfolio2").mouseover(function() {
-            $("#overlayEffect2").show();
-        }).mouseout(function() {
-            $("#overlayEffect2").hide();
-        });
-        $("#portfolio3").mouseover(function() {
-            $("#overlayEffect3").show();
-        }).mouseout(function() {
-            $("#overlayEffect3").hide();
-        });
-        $("#portfolio4").mouseover(function() {
-            $("#overlayEffect4").show();
-        }).mouseout(function() {
-            $("#overlayEffect4").hide();
-        });
-        $("#portfolio5").mouseover(function() {
-            $("#overlayEffect5").show();
-        }).mouseout(function() {
-            $("#overlayEffect5").hide();
-        });
-        $("#portfolio6").mouseover(function() {
-            $("#overlayEffect6").show();
-        }).mouseout(function() {
-            $("#overlayEffect6").hide();
-        });
-        $("#portfolio7").mouseover(function() {
-            $("#overlayEffect7").show();
-        }).mouseout(function() {
-            $("#overlayEffect7").hide();
-        });
-        $("#portfolio8").mouseover(function() {
-            $("#overlayEffect8").show();
-        }).mouseout(function() {
-            $("#overlayEffect8").hide();
-        });
-        */
     let overlayTotalNumber = (
         $(".overlay").toArray().length
     );
